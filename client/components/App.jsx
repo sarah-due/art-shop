0b89f9837ebafa8bc28ef2114e9be481d1b8a00f
sdue <sdueweke@gmail.com>
2017-11-04 15:58:06 +1300

    Set up dev environment - Hello World!

---
+++ client/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {HashRouter as Router, Route} from 'react-router-dom'
+
+const App = () => (
+  <Router>
+    <div className='app-container'>
+      <h1>Hello World</h1>
+    </div>
+  </Router>
+)
+
+export default App
---
+++ server/server.js
@@ -0,0 +1,17 @@
+var path = require('path')
+var express = require('express')
+var bodyParser = require('body-parser')
+var cors = require('cors')
+
+var server = express()
+
+server.use(cors('*'))
+
+server.use(bodyParser.json())
+server.use(express.static(path.join(__dirname, '../public')))
+
+
+module.exports = function(db) {
+  server.set('db', db)
+  return server
+}
